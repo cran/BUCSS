96573fec4644eb857ee7c06ee361a5fa0bc75021
Ken Kelley <kkelley@nd.edu>
2017-04-10 13:06:06 +0000

    version 0.0.2

---
+++ R/BUCSS.R
@@ -0,0 +1,14 @@
+#' Bias and Uncertainty Corrected Sample Size (BUCSS)
+#'
+#' BUCSS implements a method of correcting for publication bias anduncertainty when planning sample sizes in a future study from an original study
+#'
+#' Note that \url{https://designingexperiments.com} uses Shiny R apps that implement, via a web interface, the functions contained in BUCSS.
+#'
+#' @references Anderson, S. & Kelley, K., Maxwell, S. E. (revised and resubmited). Sample size planning for more accurate statistical power: A method correcting sample effect sizes for uncertainty and publication bias. \emph{Journal}, X--X.
+#'
+#' See the web page that accompanies the book here: \url{https://designingexperiments.com/}.
+#'
+#' For suggested updates, please email Samantha Anderson \email{kkelley@nd.edu} or Ken Kelley \email{kkelley@nd.edu}.
+#' @author Samantha Anderson \email{Samantha.F.Anderson.350@nd.edu} and Ken Kelley \email{kkelley@nd.edu}
+#'
+"_PACKAGE"
